046e4ef9674cf5614567a9924925f05eb304784b
jpekblue16 <pek.jenna16@gmail.com>
2023-11-22 15:21:22 -0800

    Update linked-list.js
    remove filler text

--- Javascript/linked list/linked-list.js
+++ Javascript/linked list/linked-list.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Linked List' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 class Node {
   constructor(value=0) {
     this.value = value;
